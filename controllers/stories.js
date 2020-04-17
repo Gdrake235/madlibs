9b3df1b1c03a509545d3dc1864ed0a1266ced879
Ion <ion.vizitiu88@gmail.com>
2020-04-17 15:16:50 +0100

    required form and sort

--- controllers/stories.js
+++ controllers/stories.js
@@ -2,7 +2,7 @@ var Story = require("../model/story");
 
 var StoriesController = {
   Index: function(req, res) {
-    Story.find().exec(function(err,stories) {
+    Story.find().sort({title: 1 }).exec(function(err, stories) {
     if (err) {throw err;}
     res.render('stories/index', {stories: stories})
     })
